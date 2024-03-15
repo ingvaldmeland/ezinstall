d29750b891f4c9f48248354efd30a361b68e2dcd
ingvaldmeland <ingvald.r.m@gmail.com>
2024-03-15 14:01:37 +0100

    regex fix ffs

--- ezsetup.sh
+++ ezsetup.sh
@@ -3,9 +3,9 @@
 figlet Ez Install
 
 # Install and compile applications
-sudo cp .config/pacman.conf /etc/pacman.conf
+sudo cp .config/pacman.conf /etc/pacman.conf # Enables multilib repo
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm base-devel git vim btop bat ranger
+sudo pacman -S --needed --noconfirm base-devel git vim btop bat ranger eza
 
 git clone https://aur.archlinux.org/paru.git
 pushd paru/
@@ -20,30 +20,38 @@ sudo cp .config/paru.conf /etc/paru.conf
 # popd
 # rm -rf librewolf-bin/
 
+
 # Copy config files
 ranger --copy-config=all
-cp rc.conf $HOME/.config/ranger
+cp .config/rc.conf $HOME/.config/ranger
 sudo cp .config/rc.conf /root/.config/ranger
 
-cp .vimrc $HOME
-sudo cp -a  .config/.vimrc /etc/vimrc
+cp .config/.vimrc $HOME # Use $HOME variable for compatibility with different users
+sudo cp -a .config/.vimrc /etc/vimrc
 
 
-sed -i "s/PS1='.*/PS1='\[\e[91m\][\[\e[93m\]\u\[\e[92m\]@\[\e[96m\]\H\[\e[0m\] \[\e[95m\]\w\[\e[91m\]]\[\e[0m\]\\$'/" bash.bashrc
+# Bash.rc config
+#
+# Replace PS1 prompt
+sed -i "s|PS1='.*|PS1='\\\[\\e\[91m\\\]\[\\\[\\e\[93m\\\]\\u\\\[\\e\[92m\\\]\@\\\[\\e\[96m\\\]\\H\\\[\\e\[0m\\\] \\\[\\e\[95m\\\]\\w\\\[\\e\[91m\\\]\]\\\[\\e\[0m\\\]\\\\$ '|" bash.bashrc
 
+# Adds terminal shortcuts
 printf "
 alias brc='sudo vim /etc/bash.bashrc'
 alias br='source /etc/bash.bashrc'
 
-alias vrc='vim /home/val/.vimrc && sudo cp /home/val/.vimrc /etc/vimrc'
+alias vrc='vim $HOME/.vimrc && sudo cp $HOME/.vimrc /etc/vimrc'
 
 alias r='ranger'
 alias rr='sudo ranger'
 
 alias c='clear'
 
+# Replace ls with eza
 alias ls='exa -gF --grid --icons'
-alias ll='exa -laF --header --icons'
-alias lt='exa -laF --tree --level=2 --header --icons'" >> test.txt
+alias ll='exa -laF --header --git --icons'
+alias lt='exa -laF --tree --level=2 --header --git --icons'" >> bash.bashrc
 
-source /etc/bash.bashrc
+# source /etc/bash.bashrc
+
+
